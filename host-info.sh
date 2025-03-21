7e7e7189ed861a5f262af8b43638ee5b96531020
gurutalloli <gurutalloli@gmail.com>
2025-03-21 05:36:14 +0530

    host information script updated

--- host-info.sh
+++ host-info.sh
@@ -1,4 +1,5 @@
 echo ****This is a simple script to show the host info***
 echo "Currently logged in user is `whoami` "
 echo "You are logged in on host `hostname`"
-echo "Current disk usage of / partition is `df -h / `"
+echo "Current disk usage of / partition is:"
+df -h / 
